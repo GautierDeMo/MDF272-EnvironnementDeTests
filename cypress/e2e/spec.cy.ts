f7ea9fc21c9a13d43028229a745341d2ca233b4d
SolwenG <solene.gouin@orange.fr>
2026-07-09 10:19:18 +0200

    tryout tests e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Tests Homepage de l'app", () => {
+    const pages = [
+        {label: "Get Started", path: "/register"},
+        {label: "Login", path: "/login"},
+    ];
+
+    pages.forEach(({label, path}) => {
+        it(`navigue vers ${path} en cliquant sur "${label}"`, () => {
+            cy.visit("http://localhost:5173/");
+            cy.contains("a", label).first().click({force: true});
+            cy.location("pathname").should("include", path);
+        });
+    });
+
+    it("les liens pointent vers les bonnes routes", () => {
+        cy.visit("http://localhost:5173/");
+        cy.contains("a", "Get Started").should("have.attr", "href", "/register");
+        cy.contains("a", "Login").should("have.attr", "href", "/login");
+    });
+
+    it("la page affiche le contenu correct", () => {
+        cy.visit("http://localhost:5173/");
+        cy.get("h1").should("contain.text", "TaskMaster");
+        cy.contains("Manage your tasks, tags, and subtasks").should("be.visible");
+    });
+
+
+});
